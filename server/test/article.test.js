ebcc8b8a5b928278a06f6b8cdff59168fe6bf9d2
franshiro <franshiro@gmail.com>
2018-10-18 11:40:19 +0700

    tdd add article

--- server/test/article.test.js
+++ server/test/article.test.js
@@ -8,20 +8,63 @@ const Article = require('../models/article')
 
 let article = {
     title : 'test',
-    article : 'ini adalah content article untuk test'
+    article : 'ini adalah content article untuk test',
 }
 
 let user = {
-    username: 'zura',
-    email: 'zura@mail.com',
-    password: 'zura'
-}
-
-let admin = {
-  username : 'kwonyuri',
-  email : 'yuri@gmail.com',
-  password : 'yuri'
+    email: 'zie@gmail.com',
+    password: 'zie'
 }
 
 let token;
 
+// create article
+describe('User Get Profile Data API Test', function() {
+    before(function(done) {
+        Article.deleteOne({title : 'test'})
+            .then(function(article) {
+                done();
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    });
+
+    after(function(done) {
+        Article.deleteOne({title: 'test'})
+            .then(function(result) {
+                done();
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    });
+
+    it('should return status 200 and a token', function(done) {
+        chai.request(app)
+            .post('/users/login')
+            .send(user)
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.property('token');
+                token = res.body.token;
+                done();
+            });
+    });
+
+    it('should return status 201 and article created data', function(done) {
+        chai.request(app)
+            .post('/articles/add')
+            .send(article)
+            .set('token', token)
+            .end(function(err, res) {
+                expect(res).to.have.status(201);
+                expect(res.body).to.have.property('message');
+                expect(res.body).to.have.property('article');
+                expect(res.body.article).to.have.property('title');
+                expect(res.body.article).to.have.property('article')
+                expect(res.body.article).to.have.property('_id');
+                done();
+            });
+    });
+});
