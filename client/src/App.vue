3dbb62f8b2d155065abf2dcbfe949eaf2ae17122
esadeli <efrat.sadeli@gmail.com>
2018-10-18 12:05:07 +0700

    Show List of Articles client OK

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,54 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
+    <div class="container-fluid" id="marginTopCustom">
+      <div class="row">
+        <div class="col-md-2">
+          <Sidebar :listarticles="listarticles" ></Sidebar>
+        </div>
+        <div class="col-md-10">
+          <h1>This is Article</h1>
+        </div>
+      </div>
+    </div>
+    <!--
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    -->
   </div>
 </template>
+<script>
+import axios from 'axios'
+import Navbar from '@/components/Navbar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+export default {
+  components: {
+    Navbar, Sidebar
+  },
+  data () {
+    return {
+      listarticles: []
+    }
+  },
+  created () {
+    let self = this
+    axios({
+      method: 'GET',
+      url: 'http://localhost:3009/articles/lists'
+    })
+      .then(articles => {
+        console.log('Result-----', articles.data.data)
+        self.listarticles = articles.data.data
+      })
+      .catch(error => {
+        console.log('ERROR Get List Articles ', error)
+      })
+  }
+}
+</script>
 
 <style>
 #app {
@@ -27,5 +69,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#marginTopCustom {
+  margin-top: 60px;
+  position: absolute;
 }
 </style>
---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+       <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Sidebar</h3>
+        </div>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        </form>
+        <br>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="button">Search</button>
+        <br>
+        <br>
+        <ul class="list-unstyled components">
+            <p style="color: red">List of Articles</p>
+            <hr>
+            <ul class="list-unstyled" id="homeSubmenu">
+                <li v-for="(article, index) in listarticles" :key="index">
+                  <button type="button" class="btn btn-light">{{ article.description }}</button>
+                </li>
+            </ul>
+        </ul>
+    </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: ['listarticles']
+}
+</script>
+
+<style>
+
+</style>
