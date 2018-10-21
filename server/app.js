028877c398e78a101396d01b0a7f831d3d081da3
esadeli <efrat.sadeli@gmail.com>
2018-10-22 01:44:09 +0700

    MLAB Set Up

--- server/app.js
+++ server/app.js
@@ -10,8 +10,9 @@ const ArticleRouter = require('./routes/ArticleRoutes')
 const CommentRouter = require('./routes/CommentRoutes')
 const app = express()
 
-if(process.env.NODE_ENV === 'dev'){
+if(process.env.NODE_ENV !== 'test'){
     mongoose.connect('mongodb://localhost:27017/blog23db', {useNewUrlParser: true}) 
+    // mongoose.connect(process.env.MONGO_USER, {useNewUrlParser: true })
 }else if(process.env.NODE_ENV === 'test'){
     mongoose.connect('mongodb://localhost:27017/blog23dbtesting', {useNewUrlParser: true})
 }
